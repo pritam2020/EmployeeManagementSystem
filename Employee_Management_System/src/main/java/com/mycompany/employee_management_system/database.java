d14c9f9922f353690e20f9acd3d999c2eca49314
pritam roy <pritam20002001@gmail.com>
2023-03-06 17:14:57 +0530

    initial commit

---
+++ Employee_Management_System/src/main/java/com/mycompany/employee_management_system/database.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.employee_management_system;
+
+/**
+ *
+ * @author RIVERSIDE-19
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.*;  
+public class database {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+//        try{
+//            String hostname="localhost";
+//            String instanceName="MSSQLSERVER02";
+//            String username="user1";
+//            String database="EmployeeManagementSystem";
+//            String password="user1@pass";
+//
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//            String ConnectionUrl="jdbc:sqlserver://"+hostname+":1433"+";instances="+instanceName+";databaseName="+database;
+//            Connection conn=DriverManager.getConnection(ConnectionUrl,username,password);
+//        }
+//        catch(Exception e){
+//            e.printStackTrace();
+//        }
+       
+    }
+}
+
+
+
+ 
